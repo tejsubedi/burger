ab33fa45df620c7ffd45dfc747190e7334dfb0a4
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-13 17:55:45 -0400

    schema and seeds database has been created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+insert into burgers(burger_name) values('Cheese Burger');
+insert into burgers(burger_name) values('Ham Burger');
+insert into burgers(burger_name) values('Chicken Burger');
