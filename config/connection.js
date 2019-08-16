7cec117dee6be2c11f7d90fc4ac0542cb1fdbfa2
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-16 10:35:04 -0400

    mysql connection added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection= mysql.createConnection({
+    host:"localhost",
     port: 3306,
     user: "root",
     password: "Mysql@123",
     database: "burgers_db"
   });
+}
   
   // Make connection.
   connection.connect(function(err) {
